7a56fa488f12783515478175eaf27573e459c35e
Bogdan Liusik <bodialiusik@gmail.com>
2022-10-31 23:53:59 +0200

    add kal

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+string? word;
+const string banWord = "линуск";
+do {
+    word = Console.ReadLine();
+    Console.WriteLine(word?.Replace("л", "р"));
+} while(word != banWord);
